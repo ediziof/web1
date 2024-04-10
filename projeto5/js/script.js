964d8a60e08403477189e3952a74141cac5ba7a4
ediziof <161648021+ediziof@users.noreply.github.com>
2024-04-10 17:39:54 -0300

    Add files via upload

--- projeto5/js/script.js
+++ projeto5/js/script.js
@@ -2,4 +2,21 @@ function mudarCorBox() {
     var bx = document.querySelector("#bx1");
     bx.style.backgroundColor = "blue";
     bx.style.borderRadius = "100px";
+    
+}
+
+function escopoVar(){
+    const z = 7;
+    var x = 10;
+    {
+        let x = 5;
+        if (x%2==0){
+            x=x*2;
+        }else{
+            x=x*3;
+        }
+        console.log("Dentro do bloco: " + x);
+    }
+    console.log("Fora do bloco: "+x);
+    console.log("Fora do bloco, valor de z: " + z);
 }
